f75687eada928ced9e769beffc6b79bd540c301e
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-12-07 11:32:21 +0100

    js: require, mongoDB, bodyparser, cors, express

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs/promises');
+const express = require('express');
+const bodyParser = require('body-parser')
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require('dotenv').config();
+
+// create the mongo Client to use
+const client = new MongoClient(process.env.FINAL_URL)
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(express.static("public"));
+// alle code wordt eerst uitgevoerd door middelware (bodyParser) dan in functies
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.redirect("/info.html");
+})
+
+app.listen(port, () => {
+  console.log(`My first REST API Example app listening at http://localhost:${port}`)
+})
